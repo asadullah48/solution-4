e03cae66a8aa13339808d974822e9f191082a858
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-03-25 10:26:04 +0500

    understanding
    trying to understand

---
+++ solution4.js
@@ -0,0 +1,6 @@
+var name = "\t\n Asadullah Shafique \t\n";
+// This saves the name with whitespace
+console.log(name);
+// Shows the name with whitespace
+console.log(name.trim());
+// Shows the name without whitespace
---
+++ solution4.ts
@@ -0,0 +1,6 @@
+let name: string = "\t\n Asadullah Shafique \t\n"; 
+// This saves the name with whitespace
+console.log(name); 
+// Shows the name with whitespace
+console.log(name.trim()); 
+// Shows the name without whitespace
